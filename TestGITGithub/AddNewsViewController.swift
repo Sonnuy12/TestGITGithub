e116a0e580c4c443f2b094d0371612b5a6f10355
Sonnuy12 <kazanskij516@gmail.com>
2024-12-04 19:01:56 +0300

    Закоммитил AddNews

---
+++ TestGITGithub/AddNewsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddNewsViewController.swift
+//  TestGITGithub
+//
+//  Created by сонный on 04.12.2024.
+//
+
+import UIKit
+
+class AddNewsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Новость добалена")
+    }
+}
